10c0f8fd34baf59a72dd784b4a06547df3c4959a
nikenayw <nikenwjy23@gmail.com>
2024-12-03 10:03:02 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'radiator-springs',
+  webDir: 'dist'
+};
+
+export default config;
